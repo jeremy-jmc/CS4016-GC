7e02e12577591682a13611077bcac76bfdba1ff7
JerM1609 <jeremy.matos@utec.edu.pe>
2024-07-08 14:33:09 -0500

    final exam

--- src/20. design-pinhole.py
+++ src/20. design-pinhole.py
@@ -16,13 +16,14 @@ def calculate_focal_length(sensor_width, distance_to_object, object_percentage_o
 
 
 # Parámetros
-sensor_width = 35.0  # Ancho del sensor en mm
-distance_to_object = 3000.0  # Distancia al objeto en mm (3 metros)
+sensor_width = 24.0  # Ancho del sensor en mm
+distance_to_object = 15000.0  # Distancia al objeto en mm (3 metros)
 # Porcentaje del ancho del sensor que el objeto debe ocupar
-object_percentage_on_sensor = 0.7
+object_percentage_on_sensor = 0.9
 
 # Calcular longitud focal
-focal_length = calculate_focal_length(
-    sensor_width, distance_to_object, object_percentage_on_sensor)
-print(
-    f"La longitud focal de la cámara estenopeica debe ser de {focal_length:.2f} mm.")
+focal_length = calculate_focal_length(sensor_width, 
+                                      distance_to_object, 
+                                      object_percentage_on_sensor
+                                      )
+print(f"La longitud focal de la cámara estenopeica debe ser de {focal_length:.2f} mm.")
