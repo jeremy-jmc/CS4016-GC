97510196a16869ba3452d3728ba7c656f5f22bfc
JerM1609 <jeremy.matos@utec.edu.pe>
2024-07-09 19:15:49 -0500

    fastsam teodio

--- pcs/jeremy-jeffrey-matos-cangalaya-graded-practise-4/exercise05/solution.py
+++ pcs/jeremy-jeffrey-matos-cangalaya-graded-practise-4/exercise05/solution.py
@@ -1,11 +1,85 @@
-def count_people_cars_and_bikes(full_path_input_video):
-    pass
+import cv2
+from ultralytics import YOLO
+import torch
+import supervision as sv
+import json
+import os
+import requests
+
+
+def download_model(url: str, model_path: str):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(model_path, "wb") as f:
+            f.write(response.content)
+        print(f"Modelo descargado y guardado en {model_path}")
+    else:
+        raise Exception(f"Error al descargar el modelo: {response.status_code}")
+
+
+# https://docs.ultralytics.com/tasks/segment/#models
+model_path = "yolov8s-seg.pt"
+model_url = (
+    "https://github.com/ultralytics/assets/releases/download/v8.2.0/yolov8s-seg.pt"
+)
+
+
+def count_people_cars_and_bikes(full_path_input_video: str) -> tuple:
+    if not os.path.exists(model_path):
+        download_model(model_url, model_path)
+
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # model = torch.hub.load("WongKinYiu/yolov7","custom", path_or_model=model_path,
+    #                         source='github', trust_repo=True, verbose=True)
+    # model = model.to(device)
+    # model.eval()
+
+    model = YOLO("yolov8s-seg.pt", verbose=False)
+    # print(vars(model))
+    # print(dir(model))
+    # print(json.dumps(model.names, indent=2))
+    cap = cv2.VideoCapture(full_path_input_video)
+
+    if not cap.isOpened():
+        print("Error: No se pudo abrir el video.")
+        return 0, 0, 0
+
+    person_count, bike_count, car_count = 0, 0, 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # results = model(frame_rgb)
+        # detections = results.xyxy[0].cpu().numpy()
+
+        detections = model(frame_rgb)[0].cpu().numpy().boxes
+        # print(vars(detections))
+        # print(dir(detections))
+        for cls_ in detections.cls:
+            if int(cls_) == 0:  # Clase 0 es persona
+                person_count += 1
+            elif int(cls_) == 1:  # Clase 1 es bicicleta
+                bike_count += 1
+            elif int(cls_) == 2:  # Clase 2 es auto
+                car_count += 1
+            else:
+                continue
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return person_count, bike_count, car_count
 
 
 if __name__ == "__main__":
     result = count_people_cars_and_bikes(
-        full_path_input_video="/home/someone/example-input-yolo-1.mpg",
+        full_path_input_video="../pc4-example-inputs/video-for-yolo/videoplayback.mp4",
     )
+
+    print(f"People detected: {result[0]}")
+    print(f"Bikes detected: {result[1]}")
+    print(f"Cars detected: {result[2]}")
 
 """
 -> The output result should be like this:
--- pcs/jeremy-jeffrey-matos-cangalaya-graded-practise-4/exercise06/solution.py
+++ pcs/jeremy-jeffrey-matos-cangalaya-graded-practise-4/exercise06/solution.py
@@ -1,20 +1,94 @@
+import cv2
+from ultralytics import SAM, FastSAM
+from ultralytics.models.fastsam import FastSAMPrompt
+import torch
+import supervision as sv
+import json
+import os
+import requests
+import torch
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def download_model(url: str, model_path: str):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(model_path, "wb") as f:
+            f.write(response.content)
+        print(f"Modelo descargado y guardado en {model_path}")
+    else:
+        raise Exception(f"Error al descargar el modelo: {response.status_code}")
+
+
+# https://docs.ultralytics.com/tasks/segment/#models
+model_path = "sam.pt"
+model_url = (
+    # "https://github.com/ultralytics/assets/releases/download/v8.2.0/sam_b.pt"
+    "https://github.com/ultralytics/assets/releases/download/v8.2.0/FastSAM-s.pt"
+)
+
 def highlight_people_cars_and_bikes(
-    full_path_input_image,
-    color_scale_image,
-    color_scale_people,
-    color_scale_cars,
-    color_scale_bikes,
-    full_path_output_image,
+    full_path_input_image: str,
+    color_scale_image: tuple,
+    color_scale_people: tuple,
+    color_scale_cars: tuple,
+    color_scale_bikes: tuple,
+    full_path_output_image: str,
 ):
-    pass
+    if not os.path.exists(model_path):
+        download_model(model_url, model_path)
+
+    model = FastSAM(model_path)
+    everything_results = model(full_path_input_image, device=device, retina_masks=True, imgsz=1024, conf=0.1, iou=0.9)
+    print(everything_results)
+    print(dir(everything_results))
+    
+    prompt_process = FastSAMPrompt(full_path_input_image, everything_results, device="cpu")
+    # ann = prompt_process.everything_prompt()
+
+    people = prompt_process.text_prompt(text="a photo of people")
+    cars = prompt_process.text_prompt(text="a photo of cars")
+    bikes = prompt_process.text_prompt(text="a photo of bikes")
+
+    print('=========================================================================s')
+    # print('people' in ann)
+    print(people)
+    print(cars)
+    print(bikes)
 
 
 if __name__ == "__main__":
-    highlight_people_cars_and_bikes(
-        full_path_input_image="example-1.jpg",
-        color_scale_image=(255, 255, 255),
-        color_scale_people=(255, 0, 0),
-        color_scale_cars=(0, 255, 0),
-        color_scale_bikes=(0, 0, 255),
-        full_path_output_image="detections-example-1.jpg",
-    )
+    # for im in ['example1.jpg', 'example2.jpg', 'example3.jpg']:
+    #     torch.cuda.empty_cache()
+    #     highlight_people_cars_and_bikes(
+    #         full_path_input_image=f'../pc4-example-inputs/images-for-last-exercise/{im}',
+    #         color_scale_image=(255, 255, 255),
+    #         color_scale_people=(255, 0, 0),
+    #         color_scale_cars=(0, 255, 0),
+    #         color_scale_bikes=(0, 0, 255),
+    #         full_path_output_image=f"./detections-{im}",
+    #     )
+    #     break
+
+    full_path_input_image=f'../pc4-example-inputs/images-for-last-exercise/example2.jpg'
+    im = cv2.imread(full_path_input_image)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    print(im.shape)
+
+    model = FastSAM(model_path)
+    # print(everything_results)
+    # print(dir(everything_results))
+    
+    for idx, t in enumerate(["person", "car", "bike"]):
+        everything_results = model(full_path_input_image, device=device)    # , retina_masks=True, conf=0.1, iou=0.9
+        prompt_process = FastSAMPrompt(full_path_input_image, everything_results, device="cpu")
+        ann = prompt_process.text_prompt(text=t)
+        mask = ann[0].masks.data.cpu().numpy()
+        print(mask.shape)
+        print(mask)
+        # value counts
+        print(np.unique(mask, return_counts=True))
+        # save mask as black and white image
+        cv2.imwrite(f'./{idx}.png', mask[0] * 255)
+
